2507c37494d4ac94642044c96723b74aa0a3118e
Diogo Sarti <diogosarti13@gmail.com>
2023-01-11 20:38:41 -0300

    iniciando projeto

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import br.com.diogo.desafio.dominio.Curso;
+import br.com.diogo.desafio.dominio.Mentoria;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso("curso java", "descricao curso java", 8);
+        Mentoria mentoria1 = new Mentoria("Mentoria Java", "descricao mentoria Java");
+
+        System.out.println(curso1);
+        System.out.println(mentoria1);
     }
 }
